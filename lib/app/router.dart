ed6d676aa6756c0f509e2a1e9d70a3a16c6ea937
2paperstar <lee@paperst.ar>
2025-03-28 03:47:59 +0900

    feat: add splash page

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:pot_g/app/router.gr.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Page|Layout,Route')
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
     RedirectRoute(path: '/', redirectTo: '/splash'),
+    AutoRoute(path: '/splash', page: SplashRoute.page),
   ];
 }
