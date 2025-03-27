861719db364ead2566c18b4f438486b8a52d14f2
Boseong <lee@paperst.ar>
2025-03-28 03:43:20 +0900

    feat: add router, slang (#4)

---
+++ lib/app/pot_g_app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pot_g/app/router.dart';
+import 'package:pot_g/app/values/palette.dart';
+import 'package:pot_g/gen/strings.g.dart';
+
+final _appRouter = AppRouter();
+
+class PotGApp extends StatelessWidget {
+  const PotGApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(systemNavigationBarColor: Palette.white),
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: MaterialApp.router(
+          routerConfig: _appRouter.config(),
+          locale: TranslationProvider.of(context).flutterLocale,
+          supportedLocales: AppLocaleUtils.supportedLocales,
+          localizationsDelegates: GlobalMaterialLocalizations.delegates,
+          builder:
+              (_, child) => _Providers(child: child ?? const SizedBox.shrink()),
+        ),
+      ),
+    );
+  }
+}
+
+class _Providers extends StatelessWidget {
+  const _Providers({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
