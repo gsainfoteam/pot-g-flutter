42d1f5c96403f2588012713d9d3d37a2e3e3f864
2paperstar <lee@paperst.ar>
2025-04-03 19:08:21 +0900

    feat: mocking data in main page

--- lib/app/modules/main/presentation/pages/main_page.dart
+++ lib/app/modules/main/presentation/pages/main_page.dart
@@ -1,51 +1,62 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_app_bar.dart';
-import 'package:pot_g/app/modules/common/presentation/widgets/pot_button.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_icon_button.dart';
-import 'package:pot_g/app/modules/common/presentation/widgets/pot_toggle.dart';
 import 'package:pot_g/app/modules/main/presentation/widgets/pot_list_item.dart';
+import 'package:pot_g/app/values/palette.dart';
 import 'package:pot_g/gen/assets.gen.dart';
 
 @RoutePage()
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  bool _isEnabled = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PotAppBar(
-        title: Text('내 정보'),
         actions: [
           PotIconButton(icon: Assets.icons.addPot.svg(), onPressed: () {}),
           PotIconButton(icon: Assets.icons.userCircle.svg(), onPressed: () {}),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SafeArea(
         child: Column(
           children: [
-            Text('title'),
-            const SizedBox(height: 20),
-            PotButton(
-              onPressed: () {},
-              variant: PotButtonVariant.emphasized,
-              child: Text('button'),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+              color: Palette.white,
             ),
-            const SizedBox(height: 20),
-            PotToggle(
-              value: _isEnabled,
-              onChanged: (value) {
-                setState(() => _isEnabled = value);
-              },
+            Expanded(
+              child: Container(
+                color: Palette.lightGrey,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                    child: Column(
+                      children: [
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                        const SizedBox(height: 15),
+                        PotListItem(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-            PotListItem(),
           ],
         ),
       ),
