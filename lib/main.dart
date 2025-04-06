26d65eeba37ede5043621e283263fb47413ff432
Boseong <lee@paperst.ar>
2025-04-06 16:13:00 +0900

    feat: setup playstore (#22)
    * chore: init fastlane
    
    * feat: integrate firebase
    
    * chore: change ndk version
    
    * chore: add keystore
    
    * chore: change android output directory
    
    * chore: set `ITSAppUsesNonExemptEncryption`

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:pot_g/app/di/locator.dart';
 import 'package:pot_g/app/pot_app.dart';
+import 'package:pot_g/firebase_options.dart';
 import 'package:pot_g/gen/strings.g.dart';
 
 Future<void> main() async {
   final binding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: binding);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await configureDependencies();
   runApp(TranslationProvider(child: const PotApp()));
 }
