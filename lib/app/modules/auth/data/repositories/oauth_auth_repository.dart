7da2779df69ee6eb2bddd7487b7e4d1b140c7116
Boseong <lee@paperst.ar>
2025-05-21 15:22:21 +0900

    fix: don't delete token after expired internally (#37)

--- lib/app/modules/auth/data/repositories/oauth_auth_repository.dart
+++ lib/app/modules/auth/data/repositories/oauth_auth_repository.dart
@@ -51,11 +51,6 @@ class OauthAuthRepository implements AuthRepository {
   Stream<UserEntity?> get user =>
       _tokenRepository.token.asyncMap((token) async {
         if (token == null) return null;
-        final exp = _tokenRepository.tokenExpiration;
-        if (exp != null && exp.isBefore(DateTime.now())) {
-          await _tokenRepository.deleteToken();
-          return null;
-        }
         try {
           return _parseUser(token);
         } catch (e) {
