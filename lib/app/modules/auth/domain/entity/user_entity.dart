63c4fdd7fd4966d1bee554df1ffd7ab805cb3f88
2paperstar <lee@paperst.ar>
2025-07-06 14:42:45 -0700

    feat: mocking chat repository

--- lib/app/modules/auth/domain/entity/user_entity.dart
+++ lib/app/modules/auth/domain/entity/user_entity.dart
@@ -1,7 +1,16 @@
-class UserEntity {
+import 'package:equatable/equatable.dart';
+
+class UserEntity with EquatableMixin {
   final String email;
   final String name;
   final String uuid;
 
-  UserEntity({required this.email, required this.name, required this.uuid});
+  const UserEntity({
+    required this.email,
+    required this.name,
+    required this.uuid,
+  });
+
+  @override
+  List<Object?> get props => [email, name, uuid];
 }
