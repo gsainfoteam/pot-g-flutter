3f51e04b717d96761e524eb1857b9950b71cbd71
2paperstar <lee@paperst.ar>
2025-07-06 14:22:41 -0700

    feat: chat room input ui

--- lib/app/modules/chat/presentation/pages/chat_room_page.dart
+++ lib/app/modules/chat/presentation/pages/chat_room_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:pot_g/app/modules/chat/presentation/widgets/chat_bubble.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_app_bar.dart';
+import 'package:pot_g/app/values/palette.dart';
+import 'package:pot_g/gen/assets.gen.dart';
 
 @RoutePage()
 class ChatRoomPage extends StatelessWidget {
@@ -12,7 +15,59 @@ class ChatRoomPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PotAppBar(),
-      body: Column(children: [Text('Chat Room $id')]),
+      body: Column(
+        children: [
+          Expanded(child: SingleChildScrollView(child: _ChatList())),
+          SafeArea(child: _ChatInput()),
+        ],
+      ),
+    );
+  }
+}
+
+class _ChatList extends StatelessWidget {
+  const _ChatList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ChatBubble(),
+        ChatBubble(),
+        ChatBubble(),
+        ChatBubble(),
+        ChatBubble(),
+      ],
+    );
+  }
+}
+
+class _ChatInput extends StatelessWidget {
+  const _ChatInput();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          const SizedBox(width: 12),
+          Assets.icons.clock.svg(
+            colorFilter: ColorFilter.mode(Palette.grey, BlendMode.srcIn),
+          ),
+          const SizedBox(width: 12),
+          Assets.icons.dollar.svg(
+            colorFilter: ColorFilter.mode(Palette.grey, BlendMode.srcIn),
+          ),
+          const SizedBox(width: 12),
+          Expanded(child: TextField()),
+          const SizedBox(width: 12),
+          Assets.icons.sendDiagonal.svg(
+            colorFilter: ColorFilter.mode(Palette.grey, BlendMode.srcIn),
+          ),
+          const SizedBox(width: 12),
+        ],
+      ),
     );
   }
 }
