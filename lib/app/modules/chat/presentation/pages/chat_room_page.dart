02ba5f3371fb8e779744b43b6e8e6b516e05d256
2paperstar <lee@paperst.ar>
2025-07-06 14:29:05 -0700

    feat: input style

--- lib/app/modules/chat/presentation/pages/chat_room_page.dart
+++ lib/app/modules/chat/presentation/pages/chat_room_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pot_g/app/modules/chat/presentation/widgets/chat_bubble.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_app_bar.dart';
 import 'package:pot_g/app/values/palette.dart';
+import 'package:pot_g/app/values/text_styles.dart';
 import 'package:pot_g/gen/assets.gen.dart';
 
 @RoutePage()
@@ -42,8 +43,15 @@ class _ChatList extends StatelessWidget {
   }
 }
 
-class _ChatInput extends StatelessWidget {
+class _ChatInput extends StatefulWidget {
   const _ChatInput();
+
+  @override
+  State<_ChatInput> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<_ChatInput> {
+  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +68,30 @@ class _ChatInput extends StatelessWidget {
             colorFilter: ColorFilter.mode(Palette.grey, BlendMode.srcIn),
           ),
           const SizedBox(width: 12),
-          Expanded(child: TextField()),
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              style: TextStyles.description.copyWith(
+                height: 19 / 16,
+                color: Palette.textGrey,
+              ),
+              minLines: 1,
+              maxLines: 3,
+              decoration: InputDecoration(
+                filled: true,
+                isDense: true,
+                fillColor: Palette.lightGrey,
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 12,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
           const SizedBox(width: 12),
           Assets.icons.sendDiagonal.svg(
             colorFilter: ColorFilter.mode(Palette.grey, BlendMode.srcIn),
