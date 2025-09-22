2355e41bb42677dd711cf29d0aafaf7d94e6acda
2paperstar <lee@paperst.ar>
2025-09-22 11:20:47 +0900

    feat: add interceptor

---
+++ lib/app/modules/auth/data/data_sources/remote/authorize_interceptor.dart
@@ -0,0 +1,86 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:mutex/mutex.dart';
+import 'package:pot_g/app/di/locator.dart';
+import 'package:pot_g/app/modules/auth/data/data_sources/remote/user_auth_api.dart';
+import 'package:pot_g/app/modules/auth/data/models/refresh_request_model.dart';
+import 'package:pot_g/app/modules/auth/domain/repositories/token_repository.dart';
+import 'package:pot_g/app/modules/core/data/dio/pot_dio.dart';
+
+@injectable
+class AuthorizeInterceptor extends Interceptor {
+  final TokenRepository repository;
+  static const retriedKey = '_retried';
+  final mutex = ReadWriteMutex();
+
+  AuthorizeInterceptor(this.repository);
+
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    if (options.retried) return handler.next(options);
+
+    try {
+      await mutex.acquireRead();
+      final token = await repository.token.first;
+      if (token != null) {
+        options.headers['Authorization'] = 'Bearer $token';
+      }
+      handler.next(options);
+    } finally {
+      mutex.release();
+    }
+  }
+
+  Dio getDio() => sl<PotDio>();
+  UserAuthApi get _authApi => sl<UserAuthApi>();
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    final dio = getDio();
+    final statusCode = err.response?.statusCode;
+    if (statusCode != 401) return handler.next(err);
+    final token = await repository.token.first;
+    if (token == null) return handler.next(err);
+    if (err.requestOptions.retried) return handler.next(err);
+    err.requestOptions.retried = true;
+
+    try {
+      if (!(await refresh())) return handler.next(err);
+      final retriedResponse = await dio.fetch(err.requestOptions);
+      return handler.resolve(retriedResponse);
+    } on DioException {
+      return super.onError(err, handler);
+    }
+  }
+
+  Future<bool> refresh() async {
+    if (mutex.isWriteLocked) {
+      await mutex.acquireRead();
+      mutex.release();
+      return true;
+    }
+    await mutex.acquireWrite();
+    try {
+      final token = await repository.refreshToken.first;
+      if (token == null) return false;
+      final res = await _authApi.refresh(
+        RefreshRequestModel(refreshToken: token),
+      );
+      await repository.saveToken(res.accessToken);
+      return true;
+    } catch (e) {
+      await repository.deleteToken();
+      return false;
+    } finally {
+      mutex.release();
+    }
+  }
+}
+
+extension _RequestOptionsX on RequestOptions {
+  bool get retried => extra.containsKey(AuthorizeInterceptor.retriedKey);
+  set retried(bool value) => extra[AuthorizeInterceptor.retriedKey] = value;
+}
--- lib/app/modules/core/data/dio/pot_dio.dart
+++ lib/app/modules/core/data/dio/pot_dio.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pot_g/app/modules/auth/data/data_sources/remote/authorize_interceptor.dart';
 import 'package:pot_g/app/values/config.dart';
 
 @singleton
 class PotDio extends DioForNative {
-  PotDio() : super(BaseOptions(baseUrl: Config.apiBaseUrl));
+  PotDio(AuthorizeInterceptor interceptor)
+    : super(BaseOptions(baseUrl: Config.apiBaseUrl)) {
+    interceptors.add(interceptor);
+  }
 }
