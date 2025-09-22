655ced861ee47e406f1f314777d8af6028d8a9cb
2paperstar <lee@paperst.ar>
2025-09-22 11:47:22 +0900

    fix: inject user api

--- lib/app/modules/user/data/data_source/remote/user_api.dart
+++ lib/app/modules/user/data/data_source/remote/user_api.dart
@@ -1,10 +1,15 @@
+import 'package:injectable/injectable.dart';
+import 'package:pot_g/app/modules/core/data/dio/pot_dio.dart';
 import 'package:pot_g/app/modules/user/data/models/self_user_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'user_api.g.dart';
 
+@injectable
 @RestApi(baseUrl: '/api/v1/user/')
 abstract class UserApi {
+  factory UserApi(PotDio dio) = _UserApi;
+
   @GET('info')
   Future<SelfUserModel> getUser();
 }
