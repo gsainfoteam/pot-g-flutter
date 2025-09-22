61f3088df54b627f909518b4b1de83f0db1d3994
2paperstar <lee@paperst.ar>
2025-09-22 11:08:41 +0900

    feat: bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:pot_g/app/di/locator.dart';
 import 'package:pot_g/app/pot_app.dart';
+import 'package:pot_g/app_bloc_observer.dart';
 import 'package:pot_g/firebase_options.dart';
 import 'package:pot_g/gen/strings.g.dart';
 
@@ -11,5 +14,8 @@ Future<void> main() async {
   FlutterNativeSplash.preserve(widgetsBinding: binding);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await configureDependencies();
+  if (kDebugMode) {
+    Bloc.observer = AppBlocObserver();
+  }
   runApp(TranslationProvider(child: const PotApp()));
 }
