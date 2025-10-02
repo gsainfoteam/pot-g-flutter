e537d514ea6bc99a10f6576dd79236e1df7c9b4c
2paperstar <lee@paperst.ar>
2025-10-02 14:30:11 +0900

    feat: make socket event with generic

--- lib/app/modules/socket/data/models/base_socket_response_model.dart
+++ lib/app/modules/socket/data/models/base_socket_response_model.dart
@@ -3,26 +3,35 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'base_socket_response_model.freezed.dart';
 part 'base_socket_response_model.g.dart';
 
-@freezed
-sealed class BaseSocketResponseResultModel
-    with _$BaseSocketResponseResultModel {
-  const factory BaseSocketResponseResultModel({
-    required String responseCode,
-    required String? result,
-  }) = _BaseSocketResponseResultModel;
-
-  factory BaseSocketResponseResultModel.fromJson(Map<String, dynamic> json) =>
-      _$BaseSocketResponseResultModelFromJson(json);
+abstract class BaseSocketEventModel {
+  String get type;
 }
 
-@freezed
-sealed class BaseSocketResponseModel with _$BaseSocketResponseModel {
+@Freezed(toJson: false, genericArgumentFactories: true)
+sealed class BaseSocketResponseResultModel<T extends BaseSocketEventModel>
+    with _$BaseSocketResponseResultModel<T> {
+  const factory BaseSocketResponseResultModel({
+    required String responseCode,
+    required T? result,
+  }) = _BaseSocketResponseResultModel;
+
+  factory BaseSocketResponseResultModel.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object?) fromJsonT,
+  ) => _$BaseSocketResponseResultModelFromJson(json, fromJsonT);
+}
+
+@Freezed(toJson: false, genericArgumentFactories: true)
+sealed class BaseSocketResponseModel<T extends BaseSocketEventModel>
+    with _$BaseSocketResponseModel<T> {
   const factory BaseSocketResponseModel({
     required String type,
     required String requestId,
-    required BaseSocketResponseResultModel body,
+    required BaseSocketResponseResultModel<T> body,
   }) = _BaseSocketResponseModel;
 
-  factory BaseSocketResponseModel.fromJson(Map<String, dynamic> json) =>
-      _$BaseSocketResponseModelFromJson(json);
+  factory BaseSocketResponseModel.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object?) fromJsonT,
+  ) => _$BaseSocketResponseModelFromJson(json, fromJsonT);
 }
