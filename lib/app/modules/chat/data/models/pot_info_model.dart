8a35b1c7d3a5a2a61fb5a083a42c808971d06e26
2paperstar <lee@paperst.ar>
2025-10-06 22:55:49 +0900

    fix: move pot status enum to domain layer

--- lib/app/modules/chat/data/models/pot_info_model.dart
+++ lib/app/modules/chat/data/models/pot_info_model.dart
@@ -1,8 +1,8 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pot_g/app/modules/chat/data/enums/pot_status.dart';
 import 'package:pot_g/app/modules/chat/data/models/pot_accounting_info_model.dart';
 import 'package:pot_g/app/modules/chat/data/models/pot_users_info_model.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_info_entity.dart';
+import 'package:pot_g/app/modules/chat/domain/enums/pot_status.dart';
 import 'package:pot_g/app/modules/core/data/models/route_model.dart';
 
 part 'pot_info_model.freezed.dart';
--- lib/app/modules/chat/domain/entities/pot_info_entity.dart
+++ lib/app/modules/chat/domain/entities/pot_info_entity.dart
@@ -1,5 +1,5 @@
-import 'package:pot_g/app/modules/chat/data/enums/pot_status.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_users_info_entity.dart';
+import 'package:pot_g/app/modules/chat/domain/enums/pot_status.dart';
 import 'package:pot_g/app/modules/core/domain/entities/route_entity.dart';
 
 abstract class PotInfoEntity {
