d3f018cd5fb65cd4d94209d29df6c7e7fd141594
2paperstar <lee@paperst.ar>
2025-10-06 22:11:39 +0900

    refactor: separate `groupConsecutiveBy` as extension

--- lib/app/modules/chat/presentation/pages/chat_room_page.dart
+++ lib/app/modules/chat/presentation/pages/chat_room_page.dart
@@ -10,6 +10,7 @@ import 'package:pot_g/app/modules/chat/presentation/bloc/chat_bloc.dart';
 import 'package:pot_g/app/modules/chat/presentation/widgets/chat_bubble.dart';
 import 'package:pot_g/app/modules/chat/presentation/widgets/pot_info.dart';
 import 'package:pot_g/app/modules/chat/presentation/widgets/pot_users.dart';
+import 'package:pot_g/app/modules/common/presentation/extensions/list.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_app_bar.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_icon_button.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_pressable.dart';
@@ -17,41 +18,6 @@ import 'package:pot_g/app/values/palette.dart';
 import 'package:pot_g/app/values/text_styles.dart';
 import 'package:pot_g/gen/assets.gen.dart';
 import 'package:pot_g/gen/strings.g.dart';
-
-/// 연속된 요소만 그룹화하는 함수
-/// [list]: 그룹화할 리스트
-/// [keySelector]: 그룹화 기준이 되는 키를 추출하는 함수
-///
-/// 예시: [1, 2, 13, 14, 4, 3, 23, 12]를 (x) => x % 10으로 그룹화하면
-/// [[1, 2], [13, 14], [4, 3], [23], [12]]가 됩니다.
-List<List<T>> groupConsecutiveBy<T, K>(
-  List<T> list,
-  K Function(T) keySelector,
-) {
-  if (list.isEmpty) return [];
-
-  final result = <List<T>>[];
-  List<T> currentGroup = [list.first];
-
-  for (int i = 1; i < list.length; i++) {
-    final currentItem = list[i];
-    final previousItem = list[i - 1];
-
-    // 현재 아이템과 이전 아이템의 키가 같으면 같은 그룹에 추가
-    if (keySelector(currentItem) == keySelector(previousItem)) {
-      currentGroup.add(currentItem);
-    } else {
-      // 키가 다르면 현재 그룹을 결과에 추가하고 새 그룹 시작
-      result.add(List.from(currentGroup));
-      currentGroup = [currentItem];
-    }
-  }
-
-  // 마지막 그룹 추가
-  result.add(currentGroup);
-
-  return result;
-}
 
 @RoutePage()
 class ChatRoomPage extends StatelessWidget {
@@ -136,7 +102,7 @@ class _ChatList extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ChatBloc, ChatState>(
       builder: (context, state) {
-        final chats = groupConsecutiveBy(state.chats, (chat) => chat.user.id);
+        final chats = state.chats.groupConsecutiveBy((chat) => chat.user.id);
         return Column(
           children:
               chats
