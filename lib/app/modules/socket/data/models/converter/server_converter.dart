c7deece4d5f9227f97c2d3d60a31e5de7dbddfca
2paperstar <lee@paperst.ar>
2025-10-06 23:48:24 +0900

    refactor: separate convert pot event

--- lib/app/modules/socket/data/models/converter/server_converter.dart
+++ lib/app/modules/socket/data/models/converter/server_converter.dart
@@ -6,6 +6,28 @@ import 'package:pot_g/app/modules/socket/data/models/events/send_chat_response_m
 import 'package:pot_g/app/modules/socket/data/models/pot_events/chat_v1_event.dart';
 import 'package:pot_g/app/modules/socket/data/models/pot_events/user_in_v1_event.dart';
 
+PotEventModel<E> convertPotEvent<E extends PotEvent>(
+  Map<String, dynamic> jsonData,
+) {
+  PotEventModel<Ev> change<Ev extends PotEvent>(
+    Ev Function(Map<String, dynamic>) fromJsonT,
+  ) => PotEventModel.fromJson(
+    jsonData,
+    (inner) => fromJsonT(inner as Map<String, dynamic>),
+  );
+  final data = switch (jsonData['event_type']) {
+    'chat_v1' => change(ChatV1Event.fromJson),
+    'user_in_v1' => change(UserInV1Event.fromJson),
+    _ =>
+      throw ArgumentError.value(
+        jsonData,
+        'jsonData',
+        'Unknown pot event type: ${jsonData['event_type']}',
+      ),
+  };
+  return data as PotEventModel<E>;
+}
+
 BaseServerMessageModel<T> convertServerMessage<
   T extends BaseServerMessageEvent
 >(Map<String, dynamic> jsonData) {
@@ -16,26 +38,8 @@ BaseServerMessageModel<T> convertServerMessage<
     jsonData,
     (json) => fromJsonT(json as Map<String, dynamic>),
   );
-  BaseServerMessageModel<PotEventModel<E>> changePotEvent<E extends PotEvent>(
-    E Function(Map<String, dynamic>) fromJsonT,
-  ) => BaseServerMessageModel.fromJson(
-    jsonData,
-    (json) => PotEventModel.fromJson(
-      json as Map<String, dynamic>,
-      (inner) => fromJsonT(inner as Map<String, dynamic>),
-    ),
-  );
   final data = switch (type) {
-    'pot_event_receive' => switch (jsonData['body']['event_type']) {
-      'chat_v1' => changePotEvent(ChatV1Event.fromJson),
-      'user_in_v1' => changePotEvent(UserInV1Event.fromJson),
-      _ =>
-        throw ArgumentError.value(
-          jsonData,
-          'jsonData',
-          'Unknown pot event type: ${jsonData['event_type']}',
-        ),
-    },
+    'pot_event_receive' => change(convertPotEvent),
     'request_authorization' => change(RequestAuthorizationEventModel.fromJson),
     'authorization_res' => change(AuthorizationResponseModel.fromJson),
     'send_chat_res' => change(SendChatResponseModel.fromJson),
