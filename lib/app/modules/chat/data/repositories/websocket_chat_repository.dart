2843771935327ca562724b35e1285f2204c05206
2paperstar <lee@paperst.ar>
2025-10-06 23:56:47 +0900

    feat: get chats

--- lib/app/modules/chat/data/repositories/websocket_chat_repository.dart
+++ lib/app/modules/chat/data/repositories/websocket_chat_repository.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:pot_g/app/modules/chat/data/data_sources/remote/chat_pot_api.dart';
+import 'package:pot_g/app/modules/chat/data/models/get_pot_events_query_model.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/chat_entity.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_info_entity.dart';
 import 'package:pot_g/app/modules/chat/domain/repositories/chat_repository.dart';
@@ -8,7 +9,6 @@ import 'package:pot_g/app/modules/socket/data/models/events/pot_event_model.dart
 import 'package:pot_g/app/modules/socket/data/models/events/send_chat_response_model.dart';
 import 'package:pot_g/app/modules/socket/data/models/pot_events/chat_v1_event.dart';
 import 'package:pot_g/app/modules/socket/data/models/requests/send_chat_model.dart';
-import 'package:uuid/uuid.dart';
 
 @Injectable(as: ChatRepository)
 class WebsocketChatRepository implements ChatRepository {
@@ -19,7 +19,23 @@ class WebsocketChatRepository implements ChatRepository {
 
   @override
   Future<List<ChatEntity>> getChats(PotInfoEntity pot) async {
-    return [];
+    final info = await _api.getPotInfo(pot.id);
+    final users = info.usersInfo.users;
+    final events = await _api.getPotEvents(
+      pot.id,
+      GetPotEventsQueryModel(startsFrom: DateTime.now()),
+    );
+    return events.events
+        .where((e) => e.potPk == pot.id)
+        .whereType<PotEventModel<ChatV1Event>>()
+        .map(
+          (e) => ChatEntity(
+            message: e.data.content,
+            user: users.firstWhere((u) => u.id == e.data.from),
+            createdAt: e.timestamp,
+          ),
+        )
+        .toList();
   }
 
   @override
@@ -28,16 +44,15 @@ class WebsocketChatRepository implements ChatRepository {
     final users = info.usersInfo.users;
     yield* _socket
         .createStreamFor<PotEventModel<ChatV1Event>>()
-        .where((e) => e.body.potPk == pot.id)
-        .map((e) => e.body.data)
-        .map((e) {
-          return ChatEntity(
-            id: Uuid().v4(),
-            message: e.content,
-            user: users.firstWhere((u) => u.id == e.from),
-            createdAt: DateTime.now(),
-          );
-        });
+        .map((e) => e.body)
+        .where((e) => e.potPk == pot.id)
+        .map(
+          (e) => ChatEntity(
+            message: e.data.content,
+            user: users.firstWhere((u) => u.id == e.data.from),
+            createdAt: e.timestamp,
+          ),
+        );
   }
 
   @override
--- lib/app/modules/chat/domain/entities/chat_entity.dart
+++ lib/app/modules/chat/domain/entities/chat_entity.dart
@@ -2,27 +2,20 @@ import 'package:equatable/equatable.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_user_entity.dart';
 
 class ChatEntity with EquatableMixin {
-  final String id;
   final String message;
   final PotUserEntity user;
   final DateTime createdAt;
 
   const ChatEntity({
-    required this.id,
     required this.message,
     required this.user,
     required this.createdAt,
   });
 
   static ChatEntity make(String message, PotUserEntity user) {
-    return ChatEntity(
-      id: DateTime.now().millisecondsSinceEpoch.toString(),
-      message: message,
-      user: user,
-      createdAt: DateTime.now(),
-    );
+    return ChatEntity(message: message, user: user, createdAt: DateTime.now());
   }
 
   @override
-  List<Object?> get props => [id, message, user, createdAt];
+  List<Object?> get props => [message, user, createdAt];
 }
