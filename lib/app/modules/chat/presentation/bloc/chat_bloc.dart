de3f3197a46b95d5d3f312b36e221352942c5700
2paperstar <lee@paperst.ar>
2025-10-08 23:43:25 +0900

    fix: remove pot info repository

--- lib/app/modules/chat/presentation/bloc/chat_bloc.dart
+++ lib/app/modules/chat/presentation/bloc/chat_bloc.dart
@@ -6,19 +6,15 @@ import 'package:injectable/injectable.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/chat_entity.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_info_entity.dart';
 import 'package:pot_g/app/modules/chat/domain/repositories/chat_repository.dart';
-import 'package:pot_g/app/modules/chat/domain/repositories/pot_info_repository.dart';
 
 part 'chat_bloc.freezed.dart';
 
 @injectable
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
   final ChatRepository _chatRepository;
-  final PotInfoRepository _potInfoRepository;
   late PotInfoEntity _pot;
-  StreamSubscription<PotInfoEntity>? _potInfoStreamSubscription;
 
-  ChatBloc(this._chatRepository, this._potInfoRepository)
-    : super(const ChatInitial()) {
+  ChatBloc(this._chatRepository) : super(const ChatInitial()) {
     on<ChatInit>(_onChatInit);
     on<ChatSendChat>(_onChatSendChat);
   }
@@ -26,17 +22,11 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
   Future<void> _onChatInit(ChatInit event, Emitter<ChatState> emit) async {
     emit(const ChatState.loading());
     _pot = event.pot;
-    if (_potInfoStreamSubscription != null) {
-      _potInfoStreamSubscription!.cancel();
-      _potInfoStreamSubscription = null;
-    }
-    final potStream = _potInfoRepository.getPotInfoStream(event.pot);
-    _potInfoStreamSubscription = potStream.listen((pot) => _pot = pot);
     try {
       final chats = await _chatRepository.getChats(_pot);
       emit(ChatState.loaded(chats));
       return emit.forEach(
-        potStream.asyncExpand(_chatRepository.getChatsStream),
+        _chatRepository.getChatsStream(_pot),
         onData: (chat) {
           return ChatState.loaded([...state.chats, chat]);
         },
