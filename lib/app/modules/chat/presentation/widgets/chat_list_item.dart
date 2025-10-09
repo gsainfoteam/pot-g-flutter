07d3ce1448a62e08d31804dfebd3ab16196d6368
2paperstar <lee@paperst.ar>
2025-10-10 08:52:22 +0900

    chore: add last chat message todo

--- lib/app/modules/chat/presentation/widgets/chat_list_item.dart
+++ lib/app/modules/chat/presentation/widgets/chat_list_item.dart
@@ -8,6 +8,9 @@ import 'package:pot_g/app/router.gr.dart';
 import 'package:pot_g/app/values/palette.dart';
 import 'package:pot_g/app/values/text_styles.dart';
 
+// TODO: add last chat messsage
+// deleted in https://github.com/2paperstar/pot-g-flutter/commit/402e4d75e9d55f43f242d95e5a6fb4717bfd53fd
+
 class ChatListItem extends StatelessWidget {
   const ChatListItem({super.key, required this.pot});
 
