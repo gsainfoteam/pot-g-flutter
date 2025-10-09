f1821bd52c9cbfae6e1e41bf71b911838c9f93cf
2paperstar <lee@paperst.ar>
2025-10-09 16:53:31 +0900

    refactor: use pot.route.name

--- lib/app/modules/chat/presentation/widgets/chat_list_item.dart
+++ lib/app/modules/chat/presentation/widgets/chat_list_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:pot_g/app/modules/chat/domain/enums/pot_status.dart';
 import 'package:pot_g/app/modules/core/domain/entities/pot_detail_entity.dart';
+import 'package:pot_g/app/modules/core/domain/entities/route_entity.dart';
 import 'package:pot_g/app/values/palette.dart';
 import 'package:pot_g/app/values/text_styles.dart';
 
@@ -71,7 +72,7 @@ class ChatListItem extends StatelessWidget {
                           ),
                           const SizedBox(width: 8),
                           Text(
-                            '${pot.route.from.name} → ${pot.route.to.name}',
+                            pot.route.name,
                             style: TextStyles.description.copyWith(
                               color: textColorDescription,
                             ),
