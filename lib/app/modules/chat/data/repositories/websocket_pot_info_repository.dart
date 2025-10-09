d64935751576e8a5250cd1034766a1fb6449ad03
2paperstar <lee@paperst.ar>
2025-10-09 09:04:50 +0900

    feat: subscribe for all pot events except for chat_v1 event

--- lib/app/modules/chat/data/repositories/websocket_pot_info_repository.dart
+++ lib/app/modules/chat/data/repositories/websocket_pot_info_repository.dart
@@ -4,7 +4,13 @@ import 'package:pot_g/app/modules/chat/domain/entities/pot_info_entity.dart';
 import 'package:pot_g/app/modules/chat/domain/repositories/pot_info_repository.dart';
 import 'package:pot_g/app/modules/socket/data/data_sources/websocket.dart';
 import 'package:pot_g/app/modules/socket/data/models/events/pot_event_model.dart';
+import 'package:pot_g/app/modules/socket/data/models/pot_events/accounting_confirm_v1_event.dart';
+import 'package:pot_g/app/modules/socket/data/models/pot_events/accounting_request_v1_event.dart';
+import 'package:pot_g/app/modules/socket/data/models/pot_events/archive_v1_event.dart';
+import 'package:pot_g/app/modules/socket/data/models/pot_events/departure_confirm_v1_event.dart';
 import 'package:pot_g/app/modules/socket/data/models/pot_events/user_in_v1_event.dart';
+import 'package:pot_g/app/modules/socket/data/models/pot_events/user_kick_v1_event.dart';
+import 'package:pot_g/app/modules/socket/data/models/pot_events/user_leave_v1_event.dart';
 
 @Injectable(as: PotInfoRepository)
 class WebsocketPotInfoRepository implements PotInfoRepository {
@@ -18,7 +24,16 @@ class WebsocketPotInfoRepository implements PotInfoRepository {
     yield await _api.getPotInfo(pot.id);
     yield* _socket
         .createStreamFor<PotEventModel>()
-        .where((p) => p is PotEventModel<UserInV1Event>)
+        .where(
+          (p) =>
+              p is PotEventModel<UserInV1Event> ||
+              p is PotEventModel<UserLeaveV1Event> ||
+              p is PotEventModel<UserKickV1Event> ||
+              p is PotEventModel<DepartureConfirmV1Event> ||
+              p is PotEventModel<AccountingRequestV1Event> ||
+              p is PotEventModel<AccountingConfirmV1Event> ||
+              p is PotEventModel<ArchiveV1Event>,
+        )
         .map((e) => e.body)
         .where((e) => e.potPk == pot.id)
         .asyncMap((e) => _api.getPotInfo(pot.id));
