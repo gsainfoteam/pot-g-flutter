4f957e950a807f52a3a837c6d386ca945887b7ac
2paperstar <lee@paperst.ar>
2025-10-09 11:39:36 +0900

    feat: implement set departure time

--- lib/app/modules/chat/data/repositories/websocket_pot_info_repository.dart
+++ lib/app/modules/chat/data/repositories/websocket_pot_info_repository.dart
@@ -1,5 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:pot_g/app/modules/chat/data/data_sources/remote/chat_pot_api.dart';
+import 'package:pot_g/app/modules/chat/data/enums/pot_departure_time_result.dart';
+import 'package:pot_g/app/modules/chat/data/models/confirm_departure_time_request_model.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_info_entity.dart';
 import 'package:pot_g/app/modules/chat/domain/repositories/pot_info_repository.dart';
 import 'package:pot_g/app/modules/socket/data/data_sources/websocket.dart';
@@ -40,8 +42,24 @@ class WebsocketPotInfoRepository implements PotInfoRepository {
   }
 
   @override
-  Future<void> setDepartureTime(PotInfoEntity pot, DateTime date) {
-    // TODO: implement setDepartureTime
-    throw UnimplementedError();
+  Future<void> setDepartureTime(PotInfoEntity pot, DateTime date) async {
+    final result = await _api.confirmDepartureTime(
+      pot.id,
+      ConfirmDepartureTimeRequestModel(departureTime: date),
+    );
+    switch (result.result) {
+      case PotDepartureTimeResult.ok:
+        return;
+      case PotDepartureTimeResult.notAHost:
+        throw Exception('Not a host');
+      case PotDepartureTimeResult.afterDeparture:
+        throw Exception('After departure');
+      case PotDepartureTimeResult.beforeNow:
+        throw Exception('Before now');
+      case PotDepartureTimeResult.potNotExist:
+        throw Exception('Pot not exist');
+      case PotDepartureTimeResult.potAlreadyClosed:
+        throw Exception('Pot already closed');
+    }
   }
 }
