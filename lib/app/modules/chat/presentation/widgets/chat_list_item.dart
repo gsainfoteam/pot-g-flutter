c5dbac40ad38ddd1b406a9fe776efe1044429ee8
2paperstar <lee@paperst.ar>
2025-10-10 09:13:46 +0900

    refactor: minor

--- lib/app/modules/chat/presentation/widgets/chat_list_item.dart
+++ lib/app/modules/chat/presentation/widgets/chat_list_item.dart
@@ -43,7 +43,9 @@ class ChatListItem extends StatelessWidget {
     return PotPressable(
       onTap: () => ChatRoomRoute(pot: pot).push(context),
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding:
+            EdgeInsets.symmetric(horizontal: 16) +
+            EdgeInsets.only(bottom: 16, top: 12),
         decoration: BoxDecoration(
           color: Palette.white,
           borderRadius: BorderRadius.all(Radius.circular(12)),
@@ -60,46 +62,43 @@ class ChatListItem extends StatelessWidget {
             ),
           ],
         ),
-        child: Container(
-          padding: EdgeInsets.only(bottom: 16, top: 12),
-          child: Stack(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:
-                    [
-                      Text(
-                        '팟 정보',
-                        style: TextStyles.caption.copyWith(color: Palette.grey),
-                      ),
-                      field(label: '노선', value: pot.route.name),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:
+                  [
+                    Text(
+                      '팟 정보',
+                      style: TextStyles.caption.copyWith(color: Palette.grey),
+                    ),
+                    field(label: '노선', value: pot.route.name),
+                    field(
+                      label: '날짜',
+                      value: DateFormat.yMd().add_E().format(pot.startsAt),
+                    ),
+                    field(
+                      label: '시간',
+                      value:
+                          pot.status == PotStatus.beforeConfirmed
+                              ? '${DateFormat.Hm().format(pot.startsAt)}~${DateFormat.Hm().format(pot.endsAt)}'
+                              : DateFormat.Hm().format(pot.startsAt),
+                    ),
+                    if (pot.status == PotStatus.waitAccounting ||
+                        pot.status == PotStatus.archived)
                       field(
-                        label: '날짜',
-                        value: DateFormat.yMd().add_E().format(pot.startsAt),
-                      ),
-                      field(
-                        label: '시간',
+                        label: '정산',
                         value:
-                            pot.status == PotStatus.beforeConfirmed
-                                ? '${DateFormat.Hm().format(pot.startsAt)}~${DateFormat.Hm().format(pot.endsAt)}'
-                                : DateFormat.Hm().format(pot.startsAt),
+                            '${NumberFormat('#,###').format(pot.accountingRequested)}원',
                       ),
-                      if (pot.status == PotStatus.waitAccounting ||
-                          pot.status == PotStatus.archived)
-                        field(
-                          label: '정산',
-                          value:
-                              '${NumberFormat('#,###').format(pot.accountingRequested)}원',
-                        ),
-                    ].intersperse(const SizedBox(height: 4)).toList(),
-              ),
-              Positioned(
-                right: 0,
-                top: 0,
-                child: _StatusChip(status: pot.status),
-              ),
-            ],
-          ),
+                  ].intersperse(const SizedBox(height: 4)).toList(),
+            ),
+            Positioned(
+              right: 0,
+              top: 0,
+              child: _StatusChip(status: pot.status),
+            ),
+          ],
         ),
       ),
     );
@@ -133,29 +132,25 @@ class _StatusChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: BorderRadius.circular(20),
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (status == PotStatus.confirmed) ...[
+            Icon(Icons.check, color: Palette.white, size: 20),
+            const SizedBox(width: 4),
+          ],
+          Text(
+            text,
+            style: TextStyles.description.copyWith(color: Palette.white),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (status == PotStatus.confirmed) ...[
-                Icon(Icons.check, color: Palette.white, size: 20),
-                const SizedBox(width: 4),
-              ],
-              Text(
-                text,
-                style: TextStyles.description.copyWith(color: Palette.white),
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
