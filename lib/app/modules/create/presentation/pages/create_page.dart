2d6b2413176d504a6e1523e739dcd41fa7097783
2paperstar <lee@paperst.ar>
2025-10-11 20:22:45 +0900

    feat: load pots after create pot

--- lib/app/modules/create/presentation/pages/create_page.dart
+++ lib/app/modules/create/presentation/pages/create_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pot_g/app/di/locator.dart';
+import 'package:pot_g/app/modules/chat/presentation/bloc/pot_detail_bloc.dart';
 import 'package:pot_g/app/modules/common/presentation/extensions/toast.dart';
 import 'package:pot_g/app/modules/common/presentation/utils/log_page.dart';
 import 'package:pot_g/app/modules/common/presentation/widgets/pot_app_bar.dart';
@@ -9,6 +10,7 @@ import 'package:pot_g/app/modules/core/domain/entities/route_entity.dart';
 import 'package:pot_g/app/modules/create/presentation/bloc/create_cubit.dart';
 import 'package:pot_g/app/modules/create/presentation/bloc/create_pot_bloc.dart';
 import 'package:pot_g/app/modules/create/presentation/widgets/create_form.dart';
+import 'package:pot_g/app/modules/list/presentation/bloc/pot_list_bloc.dart';
 import 'package:pot_g/app/router.gr.dart';
 import 'package:pot_g/gen/strings.g.dart';
 
@@ -45,6 +47,8 @@ class CreatePage extends StatelessWidget with LogPageStateless {
               return;
             }
             if (state.createdPotId != null) return;
+            context.read<PotListBloc>().add(PotListEvent.search());
+            context.read<PotDetailBloc>().add(PotDetailEvent.loadMyPots());
             ListRoute().push(context);
             ChatRoomRoute(id: state.createdPotId!).push(context);
           },
