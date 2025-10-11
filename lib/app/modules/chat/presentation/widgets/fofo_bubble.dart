e23a4e6097242618f9b78b87d613dd0564d72a28
Boseong <lee@paperst.ar>
2025-10-11 19:06:09 +0900

    feat: more profile images (#181)

--- lib/app/modules/chat/presentation/widgets/fofo_bubble.dart
+++ lib/app/modules/chat/presentation/widgets/fofo_bubble.dart
@@ -38,12 +38,9 @@ class FofoBubble extends StatelessWidget {
         height: 40,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(100),
-          border: Border.all(
-            color: const Color.fromARGB(255, 62, 44, 44),
-            width: 0.5,
-          ),
+          border: Border.all(color: Palette.borderGrey, width: 0.5),
         ),
-        child: ClipOval(child: Assets.images.fofo.image()),
+        child: ClipOval(child: Assets.images.fofo.svg()),
       ),
       name: context.t.chat_room.fofo.name,
       child: Column(
@@ -60,31 +57,28 @@ class FofoBubble extends StatelessWidget {
           ),
           if (message.actionButtons.isNotEmpty) const SizedBox(height: 8),
           Column(
-            children:
-                message.actionButtons
-                    .expandIndexed(
-                      (index, e) => [
-                        if (index != 0) const SizedBox(height: 8),
-                        PotButton(
-                          onPressed:
-                              e != null
-                                  ? () => onAction(e)
-                                  : () => _onUnknownAction(context),
-                          size: PotButtonSize.medium,
-                          variant:
-                              index == message.actionButtons.length - 1
-                                  ? PotButtonVariant.outlined
-                                  : null,
-                          child: Expanded(
-                            child: Text(
-                              action(context, e),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
+            children: message.actionButtons
+                .expandIndexed(
+                  (index, e) => [
+                    if (index != 0) const SizedBox(height: 8),
+                    PotButton(
+                      onPressed: e != null
+                          ? () => onAction(e)
+                          : () => _onUnknownAction(context),
+                      size: PotButtonSize.medium,
+                      variant: index == message.actionButtons.length - 1
+                          ? PotButtonVariant.outlined
+                          : null,
+                      child: Expanded(
+                        child: Text(
+                          action(context, e),
+                          textAlign: TextAlign.center,
                         ),
-                      ],
-                    )
-                    .toList(),
+                      ),
+                    ),
+                  ],
+                )
+                .toList(),
           ),
         ],
       ),
--- lib/app/modules/chat/presentation/widgets/pot_profile_image.dart
+++ lib/app/modules/chat/presentation/widgets/pot_profile_image.dart
@@ -13,42 +13,45 @@ class PotProfileImage extends StatelessWidget {
 
   Widget getImageByIndex(int index, bool inPot) {
     final images = [
-      (Assets.images.jennie, Color(0xFFD3C5FC)),
-      (Assets.images.tree, index > 3 ? Color(0xFFFFFBAD) : Color(0xFFF8F8F8)),
-      (Assets.images.geni, Color(0xFFFFE0EC)),
-      (Assets.images.us, Color(0xFFA2D2FF)),
+      (
+        Assets.images.jennie,
+        [Color(0xffd3c5fc), Color(0xffffd8f5), Color(0xffd0ddff)],
+      ),
+      (
+        Assets.images.tree,
+        [Color(0xfff8f8f8), Color(0xfff0fff4), Color(0xffe9f3ff)],
+      ),
+      (
+        Assets.images.geni,
+        [Color(0xffffe0ec), Color(0xfffff3dc), Color(0xfffdb5a6)],
+      ),
+      (
+        Assets.images.us,
+        [Color(0xffa2d2ff), Color(0xff98c2d0), Color(0xff345c80)],
+      ),
     ];
     final sample = images[index % images.length];
-    final image =
-        inPot
-            ? sample.$1.image()
-            : sample.$1.image(
-              colorBlendMode: BlendMode.color,
-              color: Palette.grey,
-            );
-    final hsl = HSLColor.fromColor(sample.$2);
-    final color =
-        hsl
-            .withHue(
-              (hsl.hue + [0, 120, 240][(index ~/ images.length) % 3]) % 360,
-            )
-            .toColor();
+    final image = inPot
+        ? sample.$1.svg()
+        : sample.$1.svg(
+            colorFilter: ColorFilter.mode(Palette.grey, BlendMode.color),
+          );
+    final color = sample.$2[index ~/ images.length];
     return Container(color: color, child: image);
   }
 
   @override
   Widget build(BuildContext context) {
-    final index =
-        pot.usersInfo.users.indexed
-            .firstWhereOrNull((e) => e.$2.id == user.id)
-            ?.$1;
+    final index = pot.usersInfo.users.indexed
+        .firstWhereOrNull((e) => e.$2.id == user.id)
+        ?.$1;
     if (index == null) return SizedBox(width: 40, height: 40);
     return Container(
       width: 40,
       height: 40,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100),
-        border: Border.all(color: Palette.borderGrey2, width: 0.5),
+        border: Border.all(color: Palette.borderGrey, width: 0.5),
       ),
       child: ClipOval(child: getImageByIndex(index, user.isInPot)),
     );
