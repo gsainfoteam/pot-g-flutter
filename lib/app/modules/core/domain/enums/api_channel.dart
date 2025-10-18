5b8c4d280e3c10808ba39723dc303c69f9d803a5
2paperstar <lee@paperst.ar>
2025-10-19 02:48:03 +0900

    feat: add appLink url in ApiChannel

--- lib/app/modules/core/domain/enums/api_channel.dart
+++ lib/app/modules/core/domain/enums/api_channel.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/foundation.dart';
 
 enum ApiChannel {
-  dev('https://api.dev.pot-g.gistory.me/', 'wss://api.dev.pot-g.gistory.me/ws'),
-  qa('https://api.qa.pot-g.gistory.me/', 'wss://api.qa.pot-g.gistory.me/ws'),
-  prod('https://api.pot-g.gistory.me/', 'wss://api.pot-g.gistory.me/ws');
+  dev(
+    'https://api.dev.pot-g.gistory.me/',
+    'wss://api.dev.pot-g.gistory.me/ws',
+    'https://dev.pot-g.gistory.me/',
+  ),
+  qa(
+    'https://api.qa.pot-g.gistory.me/',
+    'wss://api.qa.pot-g.gistory.me/ws',
+    'https://qa.pot-g.gistory.me/',
+  ),
+  prod(
+    'https://api.pot-g.gistory.me/',
+    'wss://api.pot-g.gistory.me/ws',
+    'https://pot-g.gistory.me/',
+  );
 
   final String url;
+  final String appLinkUrl;
   final String _wsUrl;
   Uri get wsUrl => Uri.parse(_wsUrl);
-  const ApiChannel(this.url, this._wsUrl);
+  const ApiChannel(this.url, this._wsUrl, this.appLinkUrl);
   factory ApiChannel.byMode() => kDebugMode ? dev : prod;
 }
