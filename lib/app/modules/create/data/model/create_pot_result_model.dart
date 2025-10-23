23fa9018e119d526dfa6c34b9268327f517823f9
2paperstar <lee@paperst.ar>
2025-10-23 19:53:45 +0900

    feat: handle create pot errors

--- lib/app/modules/create/data/model/create_pot_result_model.dart
+++ lib/app/modules/create/data/model/create_pot_result_model.dart
@@ -5,9 +5,21 @@ part 'create_pot_result_model.g.dart';
 
 @Freezed(toJson: false)
 sealed class CreatePotResultModel with _$CreatePotResultModel {
-  const factory CreatePotResultModel({required String id}) =
-      _CreatePotResultModel;
+  const factory CreatePotResultModel({
+    required String? id,
+    required CreatePotResult result,
+  }) = _CreatePotResultModel;
 
   factory CreatePotResultModel.fromJson(Map<String, dynamic> json) =>
       _$CreatePotResultModelFromJson(json);
+}
+
+@JsonEnum(fieldRename: FieldRename.pascal)
+enum CreatePotResult {
+  @JsonValue('OK')
+  ok,
+  invalidCapacity,
+  departureAvailableBeforeNow,
+  invalidDepartureAvailableTime,
+  tooFarDepartureAvailableTime,
 }
--- lib/app/modules/create/data/repositories/rest_create_pot_repository.dart
+++ lib/app/modules/create/data/repositories/rest_create_pot_repository.dart
@@ -1,7 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pot_g/app/modules/core/domain/repositories/create_pot_repository.dart';
 import 'package:pot_g/app/modules/create/data/data_source/remote/create_pot_api.dart';
 import 'package:pot_g/app/modules/create/data/model/create_pot_model.dart';
+import 'package:pot_g/app/modules/create/data/model/create_pot_result_model.dart';
+import 'package:pot_g/app/modules/create/domain/exceptions/create_pot_exception.dart';
 
 @Injectable(as: CreatePotRepository)
 class RestCreatePotRepository implements CreatePotRepository {
@@ -23,7 +26,22 @@ class RestCreatePotRepository implements CreatePotRepository {
       maxCount: maxCount,
     );
 
-    final result = await _api.createPot(createPotModel);
-    return result.id;
+    try {
+      final result = await _api.createPot(createPotModel);
+      switch (result.result) {
+        case CreatePotResult.ok:
+          return result.id!;
+        case CreatePotResult.invalidCapacity:
+          throw CreatePotException.invalidCapacity();
+        case CreatePotResult.departureAvailableBeforeNow:
+          throw CreatePotException.departureAvailableBeforeNow();
+        case CreatePotResult.invalidDepartureAvailableTime:
+          throw CreatePotException.invalidDepartureAvailableTime();
+        case CreatePotResult.tooFarDepartureAvailableTime:
+          throw CreatePotException.tooFarDepartureAvailableTime();
+      }
+    } on DioException catch (e) {
+      throw CreatePotException.networkError(e.error.toString());
+    }
   }
 }
