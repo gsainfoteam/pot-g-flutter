bf09c536cc08f9b190e196fb3066b3d6a522c878
2paperstar <lee@paperst.ar>
2025-10-23 20:01:30 +0900

    fix: endDate

--- lib/app/modules/create/presentation/widgets/create_form.dart
+++ lib/app/modules/create/presentation/widgets/create_form.dart
@@ -147,7 +147,7 @@ class _DateInput extends StatelessWidget {
     return DateSelect(
       selectedDate: selected,
       minDate: DateTime.now().startOfDay(),
-      maxDate: DateTime.now().add(const Duration(days: 14)).endOfDay(),
+      maxDate: DateTime.now().add(const Duration(days: 13)).endOfDay(),
       onSelected: (date) {
         L.c(
           'dateSelectorItem',
