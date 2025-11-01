dd9ddcfdb85cad01f8f9bafd63a82cf298d8dc07
2paperstar <lee@paperst.ar>
2025-11-01 23:06:28 +0900

    feat: log routing

--- lib/app/modules/common/presentation/utils/log_observer.dart
+++ lib/app/modules/common/presentation/utils/log_observer.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pot_g/app/modules/common/presentation/utils/log.dart';
@@ -7,6 +9,7 @@ class LogObserver extends AutoRouterObserver {
   @override
   void didPush(Route route, Route? previousRoute) {
     if (route.settings is AutoRoutePage) {
+      log('Routing: ${route.settings.name}', name: 'router');
       final page = route.settings as AutoRoutePage;
       if (page.child is LogPage) {
         final logPage = page.child as LogPage;
@@ -17,7 +20,16 @@ class LogObserver extends AutoRouterObserver {
   }
 
   @override
+  void didPop(Route route, Route? previousRoute) {
+    if (route.settings is AutoRoutePage) {
+      log('Popping from: ${route.settings.name}', name: 'router');
+    }
+    super.didPop(route, previousRoute);
+  }
+
+  @override
   void didChangeTabRoute(TabPageRoute route, TabPageRoute previousRoute) {
+    log('Changing tab to: ${route.routeInfo.name}', name: 'router');
     final pageName = route.routeInfo.meta['pageName'];
     if (pageName is String) {
       L.setCurrentPage(pageName);
@@ -27,6 +39,7 @@ class LogObserver extends AutoRouterObserver {
 
   @override
   void didInitTabRoute(TabPageRoute route, TabPageRoute? previousRoute) {
+    log('Initializing tab to: ${route.routeInfo.name}', name: 'router');
     final pageName = route.routeInfo.meta['pageName'];
     if (pageName is String) {
       L.setCurrentPage(pageName);
