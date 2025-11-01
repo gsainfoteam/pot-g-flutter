494f4fca2bda3167942fd2e6ab86c306f9fc5720
2paperstar <lee@paperst.ar>
2025-11-01 23:13:20 +0900

    fix: remove anyPush fro PushSettingEntity

--- lib/app/modules/user/domain/entities/push_setting_entity.dart
+++ lib/app/modules/user/domain/entities/push_setting_entity.dart
@@ -1,13 +1,5 @@
 abstract class PushSettingEntity {
-  const PushSettingEntity({
-    required this.anyPush,
-    required this.chatPush,
-    required this.potInOutPush,
-    required this.marketingPush,
-  });
-
-  final bool anyPush;
-  final bool chatPush;
-  final bool potInOutPush;
-  final bool marketingPush;
+  bool get chatPush;
+  bool get potInOutPush;
+  bool get marketingPush;
 }
