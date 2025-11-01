454930ad1ca8ef1f5954bc544d4f1cfcf9999d15
2paperstar <lee@paperst.ar>
2025-11-02 02:42:43 +0900

    fix: wrong condition

--- lib/app/modules/chat/presentation/widgets/accounting_button.dart
+++ lib/app/modules/chat/presentation/widgets/accounting_button.dart
@@ -22,9 +22,10 @@ class AccountingButton extends StatelessWidget {
       context.showToast(context.t.chat_room.accounting.before_confirm);
       return;
     }
-    if (departureTime.isBefore(
-      DateTime.now().subtract(const Duration(minutes: 10)),
-    )) {
+    final tenMinutesAfterDeparture = departureTime.add(
+      const Duration(minutes: 10),
+    );
+    if (DateTime.now().isBefore(tenMinutesAfterDeparture)) {
       context.showToast(
         context.t.chat_room.accounting.dutch.errors.before_departure,
       );
