2ea79a5ea658a145fb52450b8398d1580ad8d74d
2paperstar <lee@paperst.ar>
2025-11-02 19:55:38 +0900

    fix: uber app id

--- lib/app/modules/chat/domain/enums/taxi_app_type.dart
+++ lib/app/modules/chat/domain/enums/taxi_app_type.dart
@@ -1,6 +1,6 @@
 enum TaxiAppType {
   kakaoT(iOS: 'id981110422', android: 'com.kakao.taxi'),
-  uber(iOS: 'id1131342792', android: 'com.ubercab'),
+  uber(iOS: 'id368677368', android: 'com.ubercab'),
   tMoney(iOS: 'id1483433931', android: 'kr.co.tmoney.tia');
 
   final String iOS;
