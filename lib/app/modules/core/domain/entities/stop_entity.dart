173c5e632432be73ba514a28d923cb6898714dd1
2paperstar <lee@paperst.ar>
2025-11-02 19:55:32 +0900

    feat: add lat, lng in stop entity

--- lib/app/modules/core/domain/entities/stop_entity.dart
+++ lib/app/modules/core/domain/entities/stop_entity.dart
@@ -1,12 +1,6 @@
 abstract class StopEntity {
-  const StopEntity({required this.id, required this.name});
-
-  final String id;
-  final String name;
-}
-
-// NOTE: temporarily hardcoded
-extension StopEntityX on StopEntity {
-  double get latitude => 35.229439;
-  double get longitude => 126.847013;
+  String get id;
+  String get name;
+  double get lat;
+  double get lng;
 }
