acce15ff7713e1d4b45c1d8cffe5db14ec76d785
2paperstar <lee@paperst.ar>
2025-11-02 18:38:06 +0900

    feat: os notification disabled banner

--- lib/app/modules/user/data/repositories/rest_push_setting_repository.dart
+++ lib/app/modules/user/data/repositories/rest_push_setting_repository.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart'
+    as permission_handler;
 import 'package:pot_g/app/modules/user/data/data_source/remote/user_api.dart';
 import 'package:pot_g/app/modules/user/data/models/push_setting_model.dart';
 import 'package:pot_g/app/modules/user/domain/entities/push_setting_entity.dart';
@@ -28,12 +29,13 @@ class RestPushSettingRepository implements PushSettingRepository {
   }
 
   @override
-  Future<bool> checkOsNotificationPermission() {
-    return Permission.notification.status.then((status) => status.isGranted);
+  Future<bool> checkOsNotificationPermission() async {
+    final status = await permission_handler.Permission.notification.status;
+    return status == permission_handler.PermissionStatus.granted;
   }
 
   @override
   Future<void> openAppSettings() {
-    return openAppSettings();
+    return permission_handler.openAppSettings();
   }
 }
