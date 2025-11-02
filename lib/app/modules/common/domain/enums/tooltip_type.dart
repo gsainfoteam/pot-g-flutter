cc94b8e4853457a7ea2466f53c9e602e4c3e3a78
2paperstar <lee@paperst.ar>
2025-11-02 17:45:39 +0900

    feat: add tooltip repository

---
+++ lib/app/modules/common/domain/enums/tooltip_type.dart
@@ -0,0 +1,1 @@
+enum TooltipType { departureTime, accounting }
---
+++ lib/app/modules/common/domain/repositories/tooltip_repository.dart
@@ -0,0 +1,15 @@
+import 'package:pot_g/app/modules/common/domain/enums/tooltip_type.dart';
+
+abstract class TooltipRepository {
+  /// Checks whether the tooltip should be shown.
+  /// [type] is the tooltip type.
+  /// Returns true if should show, false if already shown.
+  /// Note: Detailed display conditions (e.g., pot status, user role) should be
+  /// determined by the caller. This method only checks if the tooltip has been
+  /// shown before.
+  Future<bool> shouldShowTooltip(TooltipType type);
+
+  /// Marks the tooltip as shown.
+  /// After this, shouldShowTooltip will return false.
+  Future<void> markTooltipAsShown(TooltipType type);
+}
