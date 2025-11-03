d6234355f1d13634c9d922381f3fab78eedd8a0c
2paperstar <lee@paperst.ar>
2025-11-03 20:36:48 +0900

    fix: default refresh token
    this is just for fallback

--- lib/app/modules/auth/data/repositories/oauth_rest_auth_repository.dart
+++ lib/app/modules/auth/data/repositories/oauth_rest_auth_repository.dart
@@ -64,11 +64,10 @@ class OauthRestAuthRepository implements AuthRepository {
   Future<void> signOut() async {
     try {
       final refreshToken = await _tokenRepository.refreshToken.first;
-      if (refreshToken != null) {
-        await _userAuthApi.logout(
-          LogoutRequestModel(refreshToken: refreshToken),
-        );
-      }
+      // empty string is just for fallback
+      await _userAuthApi.logout(
+        LogoutRequestModel(refreshToken: refreshToken ?? ''),
+      );
     } catch (e, stackTrace) {
       L.e(e, stackTrace);
     }
