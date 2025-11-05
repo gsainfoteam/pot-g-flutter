0f0e96b840aa718a9c8893a768956f1059b90c2b
Boseong <lee@paperst.ar>
2025-11-05 10:21:11 +0900

    fix: check login cancel router resolved (#309)

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pot_g/app/modules/auth/presentation/bloc/auth_bloc.dart';
+import 'package:pot_g/app/modules/common/presentation/utils/log.dart';
 import 'package:pot_g/app/modules/user/data/data_source/constant/term_storage.dart';
 import 'package:pot_g/app/router.gr.dart';
 
@@ -43,7 +44,13 @@ class AppRouter extends RootStackRouter {
         LoginRoute(
           onDone: () => resolver.resolveNext(true, reevaluateNext: false),
           onConsent: () => resolver.next(true),
-          onCancel: () => resolver.next(false),
+          onCancel: () {
+            if (!resolver.isResolved) {
+              resolver.next(false);
+            } else {
+              L.e('login router already resolved', StackTrace.current);
+            }
+          },
         ),
       );
     }),
