781a3cb6f0b56bf2b3495334240d2b58df5edac4
Boseong <lee@paperst.ar>
2025-11-06 11:12:08 +0900

    fix: redirect to injected link in splash page (#329)

--- lib/app/modules/splash/presentation/pages/splash_page.dart
+++ lib/app/modules/splash/presentation/pages/splash_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:pot_g/app/modules/core/presentation/bloc/app_version_bloc.dart';
+import 'package:pot_g/app/modules/core/presentation/bloc/link_bloc.dart';
 import 'package:pot_g/app/router.gr.dart';
 
 @RoutePage()
@@ -19,7 +20,11 @@ class _SplashPageState extends State<SplashPage> {
     super.initState();
     Future.delayed(const Duration(seconds: 1), () async {
       if (!mounted) return;
+      final link = context.read<LinkBloc>().state.link;
       context.router.replaceAll([ListRoute()]);
+      if (link.isNotEmpty) {
+        context.router.pushPath(link);
+      }
       context.read<AppVersionBloc>().add(const AppVersionEvent.init());
       Future.delayed(const Duration(milliseconds: 200), () {
         FlutterNativeSplash.remove();
