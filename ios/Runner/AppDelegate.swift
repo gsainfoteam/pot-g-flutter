036f004875a8a48f414974630e7fe77ed48c7152
2paperstar <lee@paperst.ar>
2025-11-18 19:36:47 +0900

    feat: remove badge when open app on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,8 @@ import UIKit
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    application.applicationIconBadgeNumber = 0;
+  }
 }
