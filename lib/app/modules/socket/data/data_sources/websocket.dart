ea351fe10e5b2c5594a02bb9785420dc451ea8a1
2paperstar <lee@paperst.ar>
2025-11-19 20:54:51 +0900

    docs: add description of `_ensureConnected`

--- lib/app/modules/socket/data/data_sources/websocket.dart
+++ lib/app/modules/socket/data/data_sources/websocket.dart
@@ -142,6 +142,7 @@ class PotGSocket implements SocketInterface {
   }
 
   /// After call this function, [_channel] is not null and connected.
+  /// this do nothing if already connected or connection is not opened.
   Future<void> _ensureConnected() async {
     if (!_shouldConnected) return;
     if (isConnected) return;
