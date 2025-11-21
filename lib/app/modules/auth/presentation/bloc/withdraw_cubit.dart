ede92106cd138bac13e229138b3a348adb0e3074
tkjftkjf <qkrwlsdn1234321@gmail.com>
2025-11-21 19:35:34 +0900

    feat: add withdraw logic to cubit

--- lib/app/modules/auth/presentation/bloc/withdraw_cubit.dart
+++ lib/app/modules/auth/presentation/bloc/withdraw_cubit.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pot_g/app/modules/auth/domain/repositories/auth_repository.dart';
+import 'package:pot_g/app/modules/common/presentation/utils/log.dart';
 
 part 'withdraw_cubit.freezed.dart';
 
 @injectable
 class WithdrawCubit extends Cubit<WithdrawState> {
-  WithdrawCubit() : super(const WithdrawState());
+  final AuthRepository _repository;
+
+  WithdrawCubit(this._repository) : super(const WithdrawState());
 
   void toggleAccountDeletionConsent(bool value) {
     emit(state.copyWith(accountDeletionConsent: value));
@@ -18,6 +22,20 @@ class WithdrawCubit extends Cubit<WithdrawState> {
 
   void toggleRestoreUnavailableConsent(bool value) {
     emit(state.copyWith(restoreUnavailableConsent: value));
+  }
+
+  Future<void> withdraw() async {
+    if (!state.allChecked) {
+      return;
+    }
+
+    emit(state.copyWith());
+
+    try {
+      await _repository.withdraw();
+    } catch (e, stackTrace) {
+      L.e(e, stackTrace);
+    }
   }
 }
 
