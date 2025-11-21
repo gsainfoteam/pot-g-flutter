60d13ffa2d99adbde03412e1c83fbbb629e71015
Hyunseung Ryu <rhseungg@gmail.com>
2025-11-21 14:53:51 +0900

    fix: remove fromJson and notate toJson to freezed decorator

--- lib/app/modules/chat/data/models/report_request_model.dart
+++ lib/app/modules/chat/data/models/report_request_model.dart
@@ -1,15 +1,11 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'report_request_model.freezed.dart';
-part 'report_request_model.g.dart';
 
-@freezed
+@Freezed(toJson: true)
 sealed class ReportRequestModel with _$ReportRequestModel {
   const factory ReportRequestModel({
     required String userPk,
     required String reason,
   }) = _ReportRequestModel;
-
-  factory ReportRequestModel.fromJson(Map<String, dynamic> json) =>
-      _$ReportRequestModelFromJson(json);
 }
