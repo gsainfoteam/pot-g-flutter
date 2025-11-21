a9653e1920d24ebb8ec761b2ec93b82c6c788b8a
Hyunseung Ryu <rhseungg@gmail.com>
2025-11-21 15:08:26 +0900

    fix: missing generate part

--- lib/app/modules/chat/data/models/report_request_model.dart
+++ lib/app/modules/chat/data/models/report_request_model.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'report_request_model.freezed.dart';
+part 'report_request_model.g.dart';
 
 @Freezed(toJson: true)
 sealed class ReportRequestModel with _$ReportRequestModel {
