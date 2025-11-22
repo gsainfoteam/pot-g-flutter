7e9abd931c2a33b2edfb39d26cf97976393387b4
tkjftkjf <qkrwlsdn1234321@gmail.com>
2025-11-22 22:28:44 +0900

    feat: refactor WithdrawConsentType enum to include getDescription method

--- lib/app/modules/auth/domain/enums/withdraw_consent_type.dart
+++ lib/app/modules/auth/domain/enums/withdraw_consent_type.dart
@@ -1,5 +1,18 @@
+import 'package:pot_g/gen/strings.g.dart';
+
 enum WithdrawConsentType {
   accountDeletion,
   potInfoDeletion,
-  restoreUnavailable,
+  restoreUnavailable;
+
+  String getDescription(Translations locale) {
+    switch (this) {
+      case WithdrawConsentType.accountDeletion:
+        return locale.profile.withdraw.consents.account_deletion;
+      case WithdrawConsentType.potInfoDeletion:
+        return locale.profile.withdraw.consents.pot_info_deletion;
+      case WithdrawConsentType.restoreUnavailable:
+        return locale.profile.withdraw.consents.restore_unavailable;
+    }
+  }
 }
