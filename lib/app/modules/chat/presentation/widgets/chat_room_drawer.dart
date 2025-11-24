bcc7dc01cbbc487e214e24a03845a27f1c6f7521
GwanLiZa <144007144+GwanLiZa@users.noreply.github.com>
2025-11-24 12:27:39 +0900

    fix: prevent leaving archived pots (#417)
    * fix: prevent leaving archived pots
    
    * refactor: resolve codderabbit's conversation

--- lib/app/modules/chat/presentation/widgets/chat_room_drawer.dart
+++ lib/app/modules/chat/presentation/widgets/chat_room_drawer.dart
@@ -84,6 +84,16 @@ class ChatRoomDrawer extends StatelessWidget {
 
   Future<void> _leave(BuildContext context) async {
     L.c('leave', properties: {'potId': pot.id, 'potName': pot.name});
+    if (pot.isArchived) {
+      showOkAlertDialog(
+        context: context,
+        title: context.t.chat_room.drawer.actions.leave.archived.title,
+        message:
+            context.t.chat_room.drawer.actions.leave.error.pot_already_closed,
+      );
+      return;
+    }
+
     if (pot.departureTime != null) {
       showOkAlertDialog(
         context: context,
